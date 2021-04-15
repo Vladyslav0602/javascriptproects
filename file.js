86ddb742cfce968eb8f8098e99a81fbeba56bfde
Vladyslav <vladyslavkutsyi@gmail.com>
2021-04-15 17:42:32 +0200

    My modified proect. lesson2

--- file.js
+++ file.js
@@ -1,20 +1,39 @@
-let money = +prompt("Ваш бюджет за месяц?",''),
-    time = prompt("Введите дату в формате YYYY-MM-DD",'');
-let appData = {
-    budget: money,
-    expenses: {},
-    optionalExpenses: {},
-    income: [],
-    timeData: time,
-    savings: false
-};
+    let money = +prompt("Ваш бюджет за месяц?",''),
+        time = prompt("Введите дату в формате YYYY-MM-DD",'');
 
-let a1 = prompt("Введите обязательную статью расходов в этом месяце",""),
-    a2 = +prompt("Во сколько обойдется?",""),
-    a3 = prompt("Введите обязательную статью расходов в этом месяце",""),
-    a4 = +prompt("Во сколько обойдется?","");
+    let appData = {
+        budget: money,
+        expenses: {},
+        optionalExpenses: {},
+        income: [],
+        timeData: time,
+        savings: false
+    };
 
-    appData.expenses.a1 = a2;
-    appData.expenses.a3 = a4;
+    for(let i=0;i<2;i++){
+        let a = prompt("Введите обязательную статью расходов в этом месяце",""),
+            b = +prompt("Во сколько обойдется?","");
 
-alert("Ваш бюджет на день:" + appData.budget/30);
+        if((typeof (a))==='string' && (typeof (a))!=null && a!='' &&
+        (typeof(b))!=null && b!= '' && a.length <50 ){
+            console.log("done");
+            appData.expenses[a] = b;
+        }else{
+                alert("Данные введены неверно.Попробуйте еще раз!");
+        let a = prompt("Введите обязательную статью расходов в этом месяце",""),
+            b = +prompt("Во сколько обойдется?","");
+        }
+    }; 
+    appData.moneyPerDay = appData.budget/30;
+
+   alert("Ваш бюджет на день:" + appData.moneyPerDay );
+
+   if(appData.moneyPerDay<300){
+       console.log("малый уровень достатка");
+   }else if (appData.moneyPerDay>300 && appData.moneyPerDay<600){
+       console.log("Средний уровень достатка");
+   }else if(appData.moneyPerDay>1000) {
+       console.log("высокий уровень дохода");
+   }else {
+       console.log("Введены екверные данные!");
+   };
